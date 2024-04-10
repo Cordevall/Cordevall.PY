3e11b2a111b4c14a7f773b43ffbd37eff860165f
eveeifyeve <eveeg1971@gmail.com>
2024-04-11 03:52:24 +1000

    feat: semi python discord bot

--- src/main.py
+++ src/main.py
@@ -5,25 +5,27 @@ from pyfiglet import figlet_format
 from termcolor import colored
 from dotenv import load_dotenv
 
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 
-@bot.event
-async def on_ready():
-    # Generate ASCII art
-    ascii_art = figlet_format("Discord bot Is Online")
-    # Print it in color
-    print(colored(ascii_art, "blue"))
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=".", intents=discord.Intents.all())
+        self.cogslist = ["cog1"]
+
+        async def on_ready(self):
+            print(" Logged in as " + self.user.name)
+            synced = await self.tree.sync()
+
+        async def setup_hook(self):
+            for ext in self.cogslist:
+                await self.load_extension("Cogs." + ext)
 
 
-@bot.command()
-async def botinfo(ctx):
-    await ctx.send("Bot Is Made By Eveeify")
-
+client = Client()
 
 # Read the token from the .env file
 load_dotenv()
 token = os.getenv("token")
 
-bot.run(token)
+client.run(token)
