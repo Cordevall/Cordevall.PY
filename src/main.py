a53913253a15d2412f535a63b8592eb09b51901e
eveeifyeve <eveeg1971@gmail.com>
2024-04-12 12:32:45 +1000

    chore: defined hello command

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents().all())
 # Define a command that responds to "hello" with "world"
 @bot.hybrid_command()
 async def hello(ctx):
+    """Says world"""
     await ctx.send("world")
 
 
